654245b97bf2694714c100a63926d2a0c8fefcab
GitNiko <galaxis.ling@gmail.com>
2017-10-18 16:56:13 +0800

    colorful histogram

--- computer_vision/histograms.py
+++ computer_vision/histograms.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np 
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./images/polygon.jpg',0)
+# img = cv2.imread('./images/zidane.jpg',0)
 # plt.hist(img.ravel(), 256, [0, 256])
 # plt.show()
 
-print(cv2.calcHist([img], [0], None, [256], [0, 256]))
+# print(cv2.calcHist([img], [0], None, [256], [0, 256]))
+
+img = cv2.imread('./images/zidane.jpg')
+color = ('b', 'g', 'r')
+for i, col in enumerate(color):
+  histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+  plt.plot(histr, color=col)
+  # plt.xlim([0, 256])
+
+plt.show()
